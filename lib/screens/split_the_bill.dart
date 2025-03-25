0f11291d06138aa546ca55154fecb7c3292ece54
Uvaiz Shah <uvaizs1@gmail.com>
2025-03-26 00:30:14 +0530

    Update split_the_bill.dart with new changes

---
+++ lib/screens/split_the_bill.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplitTheBillScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Split The Bill'),
+      ),
+      body: Center(
+        child: Text('Split The Bill Page'),
+      ),
+    );
+  }
+}
